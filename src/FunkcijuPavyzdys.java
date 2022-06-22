8b30e116defb0b546c714e373db01e9b77ea05a9
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-22 14:07:38 +0300

    Baigtas antras pavyzdys su staciakampio ploto funkcija - kaip kurti papildoma funkcija ir kaip ja naudoti

--- src/FunkcijuPavyzdys.java
+++ src/FunkcijuPavyzdys.java
@@ -1,3 +1,24 @@
-public class FunkcijuPavyzdys
-{
+public class FunkcijuPavyzdys {
+    public static void main(String[] args) {
+        double ilgis = 15.37;
+        double plotis = 3.28;
+
+        double plotas = staciakampioPlotas(ilgis, plotis);
+        System.out.println("plotas = " + plotas);
+
+        System.out.println("staciakampioPlotas(50, 2.5) = " + staciakampioPlotas(50, 2.5));
+
+
+        int x = 5;
+        int y = 10;
+
+        int plotasXY = (int) staciakampioPlotas(x, y);
+        System.out.println("plotasXY = " + plotasXY);
+    }
+
+
+    // Papildomos funkcijos
+    public static double staciakampioPlotas(double a, double b) {
+        return a * b;
+    }
 }
